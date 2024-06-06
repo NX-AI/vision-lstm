5ab06eb85b42dc4f591febc8d4fe16946e0029e3
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-06-06 20:05:16 +0200

    fix torchhub

--- vision_lstm/vision_lstm.py
+++ vision_lstm/vision_lstm.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from vision_lstm_util import interpolate_sincos, to_ntuple, VitPatchEmbed, VitPosEmbed2d, DropPath
+from .vision_lstm_util import interpolate_sincos, to_ntuple, VitPatchEmbed, VitPosEmbed2d, DropPath
 
 
 class SequenceTraversal(Enum):
