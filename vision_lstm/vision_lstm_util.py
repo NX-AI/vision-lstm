30d5d8a5f3dcf5ed014990bf9bc1f0aa2f6d87be
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-09-28 22:28:55 +0200

    fix drop_path quantization issues of torchhub models

--- vision_lstm/vision_lstm_util.py
+++ vision_lstm/vision_lstm_util.py
@@ -166,12 +166,20 @@ class DropPath(nn.Sequential):
         >>> y = standalone_droppath(torch.randn(10, 4), standalone_layer)
     """
 
-    def __init__(self, *args, drop_prob: float = 0., scale_by_keep: bool = True, stochastic_drop_prob: bool = False):
+    def __init__(
+            self,
+            *args,
+            drop_prob: float = 0.,
+            scale_by_keep: bool = True,
+            stochastic_drop_prob: bool = False,
+            drop_prob_tolerance: float = 0.01,
+    ):
         super().__init__(*args)
         assert 0. <= drop_prob < 1.
         self._drop_prob = drop_prob
         self.scale_by_keep = scale_by_keep
         self.stochastic_drop_prob = stochastic_drop_prob
+        self.drop_prob_tolerance = drop_prob_tolerance
 
     @property
     def drop_prob(self):
@@ -194,15 +202,35 @@ class DropPath(nn.Sequential):
                 return x + super().forward(x, **residual_path_kwargs)
             else:
                 return x + residual_path(x, **residual_path_kwargs)
-        # generate indices to keep (propagated through transform path)
         bs = len(x)
-        if self.stochastic_drop_prob:
-            perm = torch.empty(bs, device=x.device).bernoulli_(self.keep_prob).nonzero().squeeze(1)
-            scale = 1 / self.keep_prob
-        else:
-            keep_count = max(int(bs * self.keep_prob), 1)
-            scale = bs / keep_count
-            perm = torch.randperm(bs, device=x.device)[:keep_count]
+        # for small batchsizes its not possible to do it efficiently
+        # e.g. batchsize 2 with drop_rate=0.05 would drop 1 sample and therefore increase the drop_rate to 0.5
+        # resolution: fall back to inefficient version
+        keep_count = max(int(bs * self.keep_prob), 1)
+        # allow some level of tolerance
+        actual_keep_prob = keep_count / bs
+        drop_path_delta = self.keep_prob - actual_keep_prob
+        # if drop_path_delta > self.drop_prob_tolerance:
+        #     warnings.warn(
+        #         f"efficient stochastic depth (DropPath) would change drop_path_rate by {drop_path_delta:.4f} "
+        #         f"because the batchsize is too small to accurately drop {bs - keep_count} samples per forward pass"
+        #         f" -> forcing stochastic_drop_prob=True drop_path_rate={self.drop_prob}"
+        #     )
+
+        # inefficient drop_path
+        if self.stochastic_drop_prob or drop_path_delta > self.drop_prob_tolerance:
+            shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+            random_tensor = x.new_empty(shape).bernoulli_(self.keep_prob)
+            if self.scale_by_keep:
+                random_tensor.div_(self.keep_prob)
+            if residual_path is None:
+                return x + super().forward(x, **residual_path_kwargs) * random_tensor
+            else:
+                return x + residual_path(x, **residual_path_kwargs) * random_tensor
+
+        # generate indices to keep (propagated through transform path)
+        scale = bs / keep_count
+        perm = torch.randperm(bs, device=x.device)[:keep_count]
 
         # propagate
         if self.scale_by_keep:
